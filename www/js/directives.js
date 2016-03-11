e3119dfc9281ac093520d20d6a80dab0e0f17e43
Nadeem Yasin <nadeemyasin@Nadeems-MacBook-Pro-2.local>
2016-03-11 13:03:10 +0500

    odometer replica - partially done

--- www/js/directives.js
+++ www/js/directives.js
@@ -2,15 +2,40 @@ angular.module('app.directives', ['app.controllers'])
 
 .directive('prProductView', function (productViewCtrl) {
     return {
-        restrict: 'E', //E = element, A = attribute, C = class, M = comment         
+        restrict: 'E', //E = element, A = attribute, C = class, M = comment
         scope: {
             //@ reads the attribute value, = provides two-way binding, & works with functions
             title: '@'         },
         templateUrl: 'productFeed.html',
         controller: productViewCtrl, //Embed a custom controller in the directive
         link: function ($scope, element, attrs) {
-            
+
+        } //DOM manipulation
+    }
+})
+.directive('prNumerator', function () {
+    return {
+        restrict: 'A', //E = element, A = attribute, C = class, M = comment
+
+        link: function ($scope, element, attrs) {
+          console.log('numerating....')
+          console.log(attrs)
+          // $scope.$watch('currentProduct.amount_saved', function(newVal, oldVal){
+          //     if (newVal !== oldVal) {
+          //         // Do stuff ...
+          //         console.log('numerating....')
+          //         console.log(attrs)
+          //     }
+          // });
+          // price = attrs
+          // options = {
+	        //   toValue: price,
+          //   easing: 'linear',
+          //   duration: 2000,
+          //   delimiter: ',',
+          //   rounding: 2
+          // }}
+          // $(element).numerator( options )
         } //DOM manipulation
     }
 });
-
